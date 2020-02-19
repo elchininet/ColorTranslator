12308b9d10c31784c7da3eec1c8c6399771a1c0e
Iván Pereira Muñoz <elchininet@gmail.com>
2020-02-19 21:35:03 +0100

    Modify the demo 5 to make it more insteresting

--- src/@demo/demo5/index.js
+++ src/@demo/demo5/index.js
@@ -3,15 +3,18 @@ import './styles.scss';
 export default (ColorTranslator) => {
 
     const container = document.createElement('div');
+    const rows = 11;
+    const mult = 3;
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < rows; i++) {
 
-        const blends = ColorTranslator.blendHEX('#FF0000', '#FFFF00', 3 + i * 3);
+        const blends = ColorTranslator.blendHEX('#FF0000', '#FFFF00', mult + i * mult);
 
-        blends.forEach(blend => {
+        blends.forEach((blend, index) => {
             const box = document.createElement('div');
             box.classList.add('box', `file${i}`);
             box.style.background = blend;
+            box.innerText = index + 1;
             container.appendChild(box);
         });
 
