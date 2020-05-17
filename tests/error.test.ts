02329f22083bfa8d5e490282a3df9877f7db5da7
Iván Pereira Muñoz <elchininet@gmail.com>
2020-05-17 18:48:02 +0200

    Improve test coverage - More CMYK tests - Cover more cases of errors in the input format

--- tests/error.test.ts
+++ tests/error.test.ts
@@ -20,4 +20,20 @@ describe('Wrong inputs', (): void => {
 
     });
 
+    it('Mixed object', (): void => {
+
+        expect(() => {
+            ColorTranslator.toHEX({r: '0x25', g: 20, b: '0xFF'} as unknown as ColorInput);
+        }).toThrowError(ERRORS.NOT_ACCEPTED_OBJECT_INPUT);
+
+    });
+
+    it('Wrong hex', (): void => {
+
+        expect(() => {
+            ColorTranslator.toHEX({r: '0x255', g: '0x128', b: '0xFFF'} as unknown as ColorInput);
+        }).toThrowError(ERRORS.NOT_ACCEPTED_OBJECT_INPUT);
+
+    });
+
 });
