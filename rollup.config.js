d32ae64067e0d816562a27643879c5ce5bc206c5
Iván Pereira Muñoz <elchininet@gmail.com>
2021-10-27 11:52:20 +0200

    Created a EMS version of the package allowing backward compatibility for CJS

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import pkg from './package.json';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from "rollup-plugin-terser";
+
+export default {
+    plugins: [
+        typescript(),
+        terser()
+    ],
+    input: 'src/index.ts',
+    output: [
+        {
+            file: 'dist/web/colortranslator.js',
+            format: 'iife',
+            name: 'colortranslator'
+        },
+        { file: pkg.main, format: 'cjs' },
+        { file: pkg.module, format: 'es' }
+    ]
+};
