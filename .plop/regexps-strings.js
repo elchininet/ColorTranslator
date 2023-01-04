36e87a7b3094fb22f6e2d67974c20dbc40757cd7
Iván Pereira Muñoz <elchininet@gmail.com>
2023-01-04 14:08:06 +0100

    Generate regular expressions from templates

---
+++ .plop/regexps-strings.js
@@ -0,0 +1,118 @@
+const HEX_DIGIT = '[a-f\\d]';
+const HEX_DIGIT_DOUBLE = `${HEX_DIGIT}{2}`;
+const NUMBER_WITH_DECIMALS = '(?:\\d*\\.)?\\d+';
+const SPACE = '\\s*';
+const COMMA = `${SPACE},${SPACE}`;
+const SLASH = `${SPACE}\\/${SPACE}`;
+const HSL_DEGREES_UNITS = '(?:deg|grad|rad|turn)?';
+
+module.exports = {
+    COLOR_REGEXP_STRINGS: {
+        HEX: `
+            ^#(?:
+                    (${HEX_DIGIT})
+                    (${HEX_DIGIT})
+                    (${HEX_DIGIT})
+                    (${HEX_DIGIT})?
+                |
+                    (${HEX_DIGIT_DOUBLE})
+                    (${HEX_DIGIT_DOUBLE})
+                    (${HEX_DIGIT_DOUBLE})
+                    (${HEX_DIGIT_DOUBLE})?
+            )$
+        `,
+        RGB: `
+            ^rgba?${SPACE}\\(
+                ${SPACE}
+                (?:
+                        (${NUMBER_WITH_DECIMALS}%?)
+                        ${COMMA}
+                        (${NUMBER_WITH_DECIMALS}%?)
+                        ${COMMA}
+                        (${NUMBER_WITH_DECIMALS}%?)
+                        (?:
+                            ${COMMA}
+                            (${NUMBER_WITH_DECIMALS})
+                        )?
+                    |
+                        (${NUMBER_WITH_DECIMALS}%?)
+                        ${SPACE}
+                        (${NUMBER_WITH_DECIMALS}%?)
+                        ${SPACE}
+                        (${NUMBER_WITH_DECIMALS}%?)
+                        (?:
+                            ${SLASH}
+                            (${NUMBER_WITH_DECIMALS}%?)
+                        )?
+                )
+                ${SPACE}
+            \\)$
+        `,
+        HSL: `
+            ^hsla?${SPACE}\\(
+                ${SPACE}
+                (?:
+                        (-?${NUMBER_WITH_DECIMALS}${HSL_DEGREES_UNITS})
+                        ${COMMA}
+                        (${NUMBER_WITH_DECIMALS})%
+                        ${COMMA}
+                        (${NUMBER_WITH_DECIMALS})%
+                        (?:
+                            ${COMMA}
+                            (${NUMBER_WITH_DECIMALS})
+                        )?
+                    |
+                        (-?${NUMBER_WITH_DECIMALS}${HSL_DEGREES_UNITS})
+                        ${SPACE}
+                        (${NUMBER_WITH_DECIMALS})%
+                        ${SPACE}
+                        (${NUMBER_WITH_DECIMALS})%
+                        (?:
+                            ${SLASH}
+                            (${NUMBER_WITH_DECIMALS}%?)
+                        )?
+                )
+                ${SPACE}
+            \\)$
+        `,
+        CMYK: `
+            ^(?:device-cmyk|cmyk)
+            ${SPACE}
+            \\(${SPACE}
+                (?:
+                    (${NUMBER_WITH_DECIMALS}%?)
+                    ${COMMA}
+                    (${NUMBER_WITH_DECIMALS}%?)
+                    ${COMMA}
+                    (${NUMBER_WITH_DECIMALS}%?)
+                    ${COMMA}
+                    (${NUMBER_WITH_DECIMALS}%?)
+                    (?:
+                        ${COMMA}
+                        (${NUMBER_WITH_DECIMALS})
+                    )?
+                |
+                    (${NUMBER_WITH_DECIMALS}%?)
+                    ${SPACE}
+                    (${NUMBER_WITH_DECIMALS}%?)
+                    ${SPACE}
+                    (${NUMBER_WITH_DECIMALS}%?)
+                    ${SPACE}
+                    (${NUMBER_WITH_DECIMALS}%?)
+                    (?:
+                        ${SLASH}
+                        (${NUMBER_WITH_DECIMALS}%?)
+                    )?
+                )
+                ${SPACE}
+            \\)$
+        `
+    },
+    HSL_HUE: new RegExp(`^(-?${NUMBER_WITH_DECIMALS})(${HSL_DEGREES_UNITS})$`),
+    toRegExp: function (str, caseInsensitive = false) {
+        const stringWithoutSpaces = str.replace(/\s*/gm, '');
+        return caseInsensitive
+            ? new RegExp(stringWithoutSpaces, 'i')
+            : new RegExp(stringWithoutSpaces);
+    }
+};
---
+++ plopfile.js
@@ -0,0 +1,23 @@
+const { COLOR_REGEXP_STRINGS, HSL_HUE, toRegExp } = require('./.plop/regexps-strings');
+
+module.exports = (plop) => {
+    plop.setGenerator('Compile RegExps', {
+        description: 'Compile the regular library regular expressions from a template file',
+        prompts: [],
+        actions: [
+            {
+                type: 'modify',
+                path: 'src/constants/regexps.ts',
+                pattern: /\/\/ START REGEXPS[\s\S]*\/\/ END REGEXPS/,
+                data: {
+                    HEX: toRegExp(COLOR_REGEXP_STRINGS.HEX, true),
+                    RGB: toRegExp(COLOR_REGEXP_STRINGS.RGB),
+                    HSL: toRegExp(COLOR_REGEXP_STRINGS.HSL),
+                    CMYK: toRegExp(COLOR_REGEXP_STRINGS.CMYK),
+                    HSL_HUE
+                },
+                templateFile: '.plop/regexps.hbs'
+            }
+        ]
+    });
+};
