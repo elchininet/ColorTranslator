a0c214e31b9f6ba4b86df88478aeb9487055d707
jeronimoek <jeronimoek@gmail.com>
2023-01-18 22:00:22 -0300

    Reformat functions, fix eslint and ts config

--- src/color/css.ts
+++ src/color/css.ts
@@ -5,15 +5,36 @@ import {
     HSLObject,
     CMYKObject
 } from '@types';
-import {
-    toHEX,
-    hasProp,
-    round
-} from '#helpers';
+import { toHEX, hasProp, round, roundAll, toHEXAll } from '#helpers';
 
 export const CSS = {
-    [ColorModel.HEX]: (color: HEXObject | RGBObject): string => `#${toHEX(color.r)}${toHEX(color.g)}${toHEX(color.b)}${hasProp(color, 'a') && toHEX(color.a) || ''}`,
-    [ColorModel.RGB]: (color: RGBObject): string => `rgb${hasProp<RGBObject>(color, 'a') && 'a' || ''}(${round(color.r)},${round(color.g)},${round(color.b)}${hasProp<RGBObject>(color, 'a') && `,${round(color.a, MIN_DECIMALS)}` || ''})`,
-    [ColorModel.HSL]: (color: HSLObject): string => `hsl${hasProp<HSLObject>(color, 'a') && 'a' || ''}(${round(color.h)},${round(color.s)}%,${round(color.l)}%${hasProp<HSLObject>(color, 'a') && `,${round(color.a, MIN_DECIMALS)}` || ''})`,
-    [ColorModel.CMYK]: (color: CMYKObject): string => `cmyk(${round(color.c)}%,${round(color.m)}%,${round(color.y)}%,${round(color.k)}%${hasProp<CMYKObject>(color, 'a') && `,${round(color.a, MIN_DECIMALS)}` || ''})`
+    [ColorModel.HEX]: (color: HEXObject | RGBObject): string => {
+        const { r, g, b } = color;
+        const values = toHEXAll([r, g, b]);
+        const hasAlpha = hasProp(color, 'a');
+        const alphaText = hasAlpha ? toHEX(color.a) : '';
+        return `#${values.join('')}${alphaText}`;
+    },
+    [ColorModel.RGB]: (color: RGBObject): string => {
+        const { r, g, b } = color;
+        const values = roundAll([r, g, b]);
+        const hasAlpha = hasProp<RGBObject>(color, 'a');
+        const alphaText = hasAlpha ? `,${round(color.a, MIN_DECIMALS)}` : '';
+        return `rgb${hasAlpha ? 'a' : ''}(${values.join(',')}${alphaText})`;
+    },
+    [ColorModel.HSL]: (color: HSLObject): string => {
+        const { h, s, l } = color;
+        const hasAlpha = hasProp<HSLObject>(color, 'a');
+        const alphaText = hasAlpha ? `,${round(color.a, MIN_DECIMALS)}` : '';
+        return `hsl${hasAlpha ? 'a' : ''}(${round(h)},${round(s)}%,${round(
+            l
+        )}%${alphaText})`;
+    },
+    [ColorModel.CMYK]: (color: CMYKObject): string => {
+        const { c, m, y, k } = color;
+        const values = roundAll([c, m, y, k]);
+        const hasAlpha = hasProp<CMYKObject>(color, 'a');
+        const alphaText = hasAlpha ? `,${round(color.a, MIN_DECIMALS)}` : '';
+        return `cmyk(${values.join('%,')}%${alphaText})`;
+    }
 };
