8aad1eca78903ebc22874bf2941401f68aefb8b3
Iván Pereira Muñoz <elchininet@gmail.com>
2025-06-03 18:53:10 +0200

    Refactor LCH tests with more cases

--- tests/10 - lch.test.ts
+++ tests/10 - lch.test.ts
@@ -1,36 +1,124 @@
 import { ColorTranslator } from '../src';
+import { COLORS } from './tests.constants';
 
-describe('conversion from a LCH color', () => {
+COLORS.forEach((item): void => {
 
-    const TEST_CASES = [
-        {
-            input: 'lch(54.291734 106.838999 40.852613 / 0.5)',
-            expect: '#FF0000'
-        },
-        {
-            input: 'lch(87.818128 113.339731 134.391246 / 0.5)',
-            expect: '#00FF00'
-        },
-        {
-            input: 'lch(29.567573 131.207085 301.368541 / 50%)',
-            expect: '#0000FF'
-        },
-        {
-            input: 'lch(60.169696 111.407731 327.109357 / 50%)',
-            expect: '#FF00FF'
-        }
-    ];
+    describe(`LCH tests for ${item.HEX}`, (): void => {
 
-    TEST_CASES.forEach((testCase) => {
+        it('Static LCH conversion and back from CSS string', () => {
 
-        it(`input ${testCase.input} should be ${testCase.expect}`, () => {
-
-            const instance = new ColorTranslator(testCase.input);
-            expect(instance.HEX).toBe(testCase.expect);
-            expect(instance.HEXA).toBe(`${testCase.expect}80`);
+            const lch = ColorTranslator.toLCH(item.HEX);
+            const lcha = ColorTranslator.toLCHA(item.HEX);
+            const hex = ColorTranslator.toHEX(lch);
+            const hexa = ColorTranslator.toHEXA(lcha);
+            expect(hex).toBe(item.HEX);
+            expect(hexa).toBe(item.HEXA);
 
         });
 
+        it('Static LCH conversion and back from object', () => {
+
+            const lch = ColorTranslator.toLCHObject(item.HEX);
+            const lcha = ColorTranslator.toLCHAObject(item.HEX);
+            const hex = ColorTranslator.toHEXObject(lch);
+            const hexa = ColorTranslator.toHEXAObject(lcha);
+            expect(hex).toMatchObject(item.HEXObject);
+            expect(hexa).toMatchObject(item.HEXAObject);
+
+        });
+
+        it('legacyCSS should be ignored', () => {
+
+            const lch = ColorTranslator.toLCH(item.HEX);
+            const lchLegacy = ColorTranslator.toLCH(item.HEX, { legacyCSS: true });
+            const lchLegacyWithAlphaInPercent = ColorTranslator.toLCHA(item.HEX, { legacyCSS: true, alphaUnit: 'percent' });
+            expect(lch).toBe(lchLegacy);
+            expect(lchLegacyWithAlphaInPercent.endsWith('/ 100%)')).toBe(true);
+
+        });
+
+    });
+
+});
+
+describe('LCH with percentages', (): void => {
+
+    it('L and C in percentages should be the same as a number', () => {
+
+        const percentages = [0, 25, 50, 75, 100];
+
+        percentages.forEach((value: number): void => {
+
+            const C = 150 * (value / 100);
+            const H = Math.floor(Math.random() * 360);
+
+            expect(
+                ColorTranslator.toHEX(`lch(${value} ${C} ${H})`)
+            ).toBe(
+                ColorTranslator.toHEX(`lch(${value}% ${value}% ${H})`)
+            );
+
+        });
+
+    });
+
+});
+
+describe('LCH with alpha', (): void => {
+
+    it('Alpha by default should be 1 if not specified', () => {
+        expect(
+            ColorTranslator.toLCHA('rgb(255 0 0)', { decimals: 1 }).endsWith('/ 1)')
+        ).toBe(
+            true
+        );
+        expect(
+            ColorTranslator.toLCHAObject('rgb(255 0 0)', { decimals: 1 })
+        ).toHaveProperty(
+            'A',
+            1
+        );
+    });
+
+    it('Alpha with a number should be maintained', () => {
+        expect(
+            ColorTranslator.toLCHA('rgb(255 0 0 / 0.5)', { decimals: 1 }).endsWith('/ 0.5)')
+        ).toBe(
+            true
+        );
+        expect(
+            ColorTranslator.toLCHAObject('rgb(255 0 0 / 0.5)', { decimals: 1 })
+        ).toHaveProperty(
+            'A',
+            0.5
+        );
+    });
+
+    it('Alpha with a percentage should be maintained', () => {
+        expect(
+            ColorTranslator.toLCHA('rgb(255 0 0 / 50%)', { decimals: 1 }).endsWith('/ 50%)')
+        ).toBe(
+            true
+        );
+        expect(
+            ColorTranslator.toLCHAObject('rgb(255 0 0 / 50%)', { decimals: 1 })
+        ).toHaveProperty(
+            'A',
+            0.5
+        );
+    });
+
+    it('Alpha should not be present in non-alpha methods', () => {
+        expect(
+            ColorTranslator.toLCH('rgb(255 0 0 / 1)', { decimals: 1 }).endsWith('/ 1)')
+        ).toBe(
+            false
+        );
+        expect(
+            ColorTranslator.toLCHObject('rgb(255 0 0 / 0.5)', { decimals: 1 })
+        ).not.toHaveProperty(
+            'A'
+        );
     });
 
 });
