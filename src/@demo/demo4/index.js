1853ec3eebdbb7eb5d224ca93e2ac6868dbdf05e
Iván Pereira Muñoz <elchininet@gmail.com>
2025-06-05 23:49:52 +0200

    Add hwb and lch colors to one of the demo examples

--- src/@demo/demo4/index.js
+++ src/@demo/demo4/index.js
@@ -14,11 +14,15 @@ export default (ColorTranslator) => {
 
         const rgb = ColorTranslator.toRGB(color);
         const hsl = ColorTranslator.toHSL(color, { decimals: 0 });
+        const hwb = ColorTranslator.toHWB(color, { decimals: 0 });
         const lab = ColorTranslator.toCIELab(color, { decimals: 0 });
+        const lch = ColorTranslator.toLCH(color, { decimals: 0 });
 
         const rgbDiv = document.createElement('div');
         const hslDiv = document.createElement('div');
+        const hwbDiv = document.createElement('div');
         const labDiv = document.createElement('div');
+        const lchDiv = document.createElement('div');
 
         rgbDiv.style.backgroundColor = rgb;
         rgbDiv.textContent = rgb;
@@ -26,12 +30,20 @@ export default (ColorTranslator) => {
         hslDiv.style.backgroundColor = hsl;
         hslDiv.textContent = hsl;
 
+        hwbDiv.style.backgroundColor = hwb;
+        hwbDiv.textContent = hwb;
+
         labDiv.style.backgroundColor = lab;
         labDiv.textContent = lab;
 
+        lchDiv.style.backgroundColor = lch;
+        lchDiv.textContent = lch;
+
         container.appendChild(rgbDiv);
         container.appendChild(hslDiv);
+        container.appendChild(hwbDiv);
         container.appendChild(labDiv);
+        container.appendChild(lchDiv);
 
     });
 
